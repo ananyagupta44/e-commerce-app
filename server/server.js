8e6b3ce1f4d5e0a743023c6eae5c5030189f096c
ananyagupta44 <ananyag.0801@gmail.com>
2026-06-01 12:02:29 +0530

    Configure production API URLs

--- server/server.js
+++ server/server.js
@@ -38,6 +38,10 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
